07cdccf6335d8ab0866c43741208e4d170b49f51
Chetan <ckalia@uwaterloo.ca>
2024-11-05 14:29:33 -0500

    added redis service to cache visited pages

--- api/main.py
+++ api/main.py
@@ -3,10 +3,15 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import redis
+import json
+import os
 
 from linkextractor import extract_links
 
+
 app = Flask(__name__)
+r = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 
 @app.route("/")
 def index():
@@ -17,8 +22,13 @@ def api(url):
     qs = request.query_string.decode("utf-8")
     if qs != "":
         url += "?" + qs
-    links = extract_links(url)
-    return jsonify(links)
+    # check redis cache
+    json_links = r.get(url)
+    if not json_links:
+        links = extract_links(url)
+        json_links = json.dumps(links, indent=2)
+        r.set(url, json_links)
+    return json_links
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
