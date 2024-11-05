00354921b94278c6b0b22c7821c36eaa0f1e32fe
Chetan <ckalia@uwaterloo.ca>
2024-11-05 12:22:39 -0500

    added dockerfile for web service and modifed push_to_hub script for both images

--- api/push_to_hub.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-GIT_COMMIT=$(git log -1 --format=%h)
-docker build -t ckalia/linkextractor:$GIT_COMMIT .
-docker push ckalia/linkextractor:$GIT_COMMIT
---
+++ push_to_hub.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GIT_COMMIT=$(git log -1 --format=%h)
+docker build -t ckalia/linkextractor-api:$GIT_COMMIT ./api
+docker build -t ckalia/linkextractor-web:$GIT_COMMIT ./web
+docker push ckalia/linkextractor-api:$GIT_COMMIT
+docker push ckalia/linkextractor-web:$GIT_COMMIT
