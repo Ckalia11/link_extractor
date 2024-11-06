4198e0bbb499e82d002f8ea9dc81de8aa8e219d0
Chetan <ckalia@uwaterloo.ca>
2024-11-05 21:00:50 -0500

    added scripts to .gitignore

--- push_to_hub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GIT_COMMIT=$(git log -1 --format=%h)
-docker build -t ckalia/linkextractor-api:$GIT_COMMIT ./api
-docker build -t ckalia/linkextractor-web:$GIT_COMMIT ./web
-docker push ckalia/linkextractor-api:$GIT_COMMIT
-docker push ckalia/linkextractor-web:$GIT_COMMIT
-docker rmi ckalia/linkextractor-api:$GIT_COMMIT
-docker rmi ckalia/linkextractor-web:$GIT_COMMIT
